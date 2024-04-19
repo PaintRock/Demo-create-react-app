93826c0e8a5ebdad2873b6566771861b6d741573
PaintRock <ann.knode@atlasschool.com>
2024-04-19 21:05:31 +0000

    done2day

---
+++ display-demo/src/api/axiosConfig.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: "https://api.themoviedb.org/3/movie/550?api_key=21022ee1bf657e32c52ec10f528cfaab",
+});
+
+
---
+++ display-demo/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = ({movies}) => {
+  return (
+    <div>
+      Welcome to the Home page!
+      </div>
+  )
+}
+
+export default Home
